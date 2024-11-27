fe4b0fa64261fcd2ccb337cfe1eaf98936669397
nasoviva <nasoviva@gmail.com>
2024-11-27 14:16:32 -0300

    feat: add categories for gift page cards

--- christmas-shop/pages/home/index.js
+++ christmas-shop/pages/home/index.js
@@ -3,6 +3,8 @@ const data = {
         {
             "name": "Bug Magnet",
             "description": "Able to find bugs in code like they were placed there on purpose.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+500",
@@ -15,6 +17,8 @@ const data = {
         {
             "name": "Console.log Guru",
             "description": "Uses console.log like a crystal ball to find any issue.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+500",
@@ -27,6 +31,8 @@ const data = {
         {
             "name": "Shortcut Cheater",
             "description": "Knows every keyboard shortcut like they were born with them.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+500",
@@ -39,6 +45,8 @@ const data = {
         {
             "name": "Merge Master",
             "description": "Merges branches in Git without conflicts, like a wizard during an exam.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+200",
@@ -51,6 +59,8 @@ const data = {
         {
             "name": "Async Tamer",
             "description": "Handles asynchronous code and promises like well-trained pets.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+100",
@@ -63,6 +73,8 @@ const data = {
         {
             "name": "CSS Tamer",
             "description": "Can make Flexbox and Grid work together like they were always best friends.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+200",
@@ -75,6 +87,8 @@ const data = {
         {
             "name": "Time Hacker",
             "description": "Writes code at the last moment but always meets the deadline.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+500",
@@ -87,6 +101,8 @@ const data = {
         {
             "name": "Layout Master",
             "description": "Creates perfect layouts on the first try, like they can read the designer's mind.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+500",
@@ -99,6 +115,8 @@ const data = {
         {
             "name": "Documentation Whisperer",
             "description": "Understands cryptic documentation as if they wrote it themselves.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+500",
@@ -111,6 +129,8 @@ const data = {
         {
             "name": "Feedback Master",
             "description": "Accepts client revisions with the Zen calm of Buddha.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+300",
@@ -123,6 +143,8 @@ const data = {
         {
             "name": "Code Minimalist",
             "description": "Writes code so concise that one line does more than a whole file.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+500",
@@ -135,6 +157,8 @@ const data = {
         {
             "name": "Pixel-Perfect Magician",
             "description": "Aligns elements to the last pixel, even when the design looks abstract.",
+            "img": "../../assets/images/gift-for-work.png",
+            "class": "for-work",
             "category": "For Work",
             "superpowers": {
                 "live": "+500",
@@ -147,6 +171,8 @@ const data = {
         {
             "name": "Posture Levitation",
             "description": "Can sit for hours, but maintains perfect posture like a ballerina.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+400",
@@ -159,6 +185,8 @@ const data = {
         {
             "name": "Step Master",
             "description": "Gets 10,000 steps a day even while sitting at the computer.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+400",
@@ -171,6 +199,8 @@ const data = {
         {
             "name": "Snack Resister",
             "description": "Ignoring desktop snacks like a strict dietician.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+400",
@@ -183,6 +213,8 @@ const data = {
         {
             "name": "Hydration Bot",
             "description": "Drinks the recommended 2 liters of water a day like a health-programmed robot.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+500",
@@ -195,6 +227,8 @@ const data = {
         {
             "name": "Sleep Overlord",
             "description": "Sleeps 6 hours but feels like they had 10.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+400",
@@ -207,6 +241,8 @@ const data = {
         {
             "name": "Break Guru",
             "description": "Takes a stretch break every hour without forgetting, no matter how focused.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+300",
@@ -219,6 +255,8 @@ const data = {
         {
             "name": "Eye Protector",
             "description": "Can work all day at the monitor without feeling like their eyes are on fire.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+100",
@@ -231,6 +269,8 @@ const data = {
         {
             "name": "Stress Dodger",
             "description": "Masters meditation right at the keyboard.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+100",
@@ -243,6 +283,8 @@ const data = {
         {
             "name": "Yoga Coder",
             "description": "Easily switches from coding to yoga and back.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+400",
@@ -255,6 +297,8 @@ const data = {
         {
             "name": "Healthy Snacker",
             "description": "Always picks fruit, even when chocolate is within arm’s reach.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+400",
@@ -267,6 +311,8 @@ const data = {
         {
             "name": "Chair Exerciser",
             "description": "Manages to work out without leaving the chair.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+500",
@@ -279,6 +325,8 @@ const data = {
         {
             "name": "Caffeine Filter",
             "description": "Drinks coffee at night and still falls asleep with no problem.",
+            "img": "../../assets/images/gift-for-health.png",
+            "class": "for-health",
             "category": "For Health",
             "superpowers": {
                 "live": "+400",
@@ -291,6 +339,8 @@ const data = {
         {
             "name": "Joy Charger",
             "description": "Finds joy in the little things—even in a build that finishes unexpectedly fast.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+200",
@@ -303,6 +353,8 @@ const data = {
         {
             "name": "Error Laugher",
             "description": "Laughs at code errors like they’re jokes instead of getting angry.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+300",
@@ -315,6 +367,8 @@ const data = {
         {
             "name": "Bug Acceptance Guru",
             "description": "Accepts bugs as part of the journey to perfection — it’s just another task.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+300",
@@ -327,6 +381,8 @@ const data = {
         {
             "name": "Spontaneous Coding Philosopher",
             "description": "Philosophically accepts any client suggestion after a long refactor.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+300",
@@ -339,6 +395,8 @@ const data = {
         {
             "name": "Deadline Sage",
             "description": "Remains zen even when the deadline is close and the project manager is stressed.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+200",
@@ -351,6 +409,8 @@ const data = {
         {
             "name": "Inspiration Maestro",
             "description": "Finds inspiration on an empty screen as if masterpieces are already there.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+300",
@@ -363,6 +423,8 @@ const data = {
         {
             "name": "Peace Keeper",
             "description": "Maintains inner calm even in moments of intense crisis.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+200",
@@ -375,6 +437,8 @@ const data = {
         {
             "name": "Empathy Guru",
             "description": "Feels the team’s mood and can lift everyone’s spirits.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+500",
@@ -387,6 +451,8 @@ const data = {
         {
             "name": "Laughter Generator",
             "description": "Can lighten any tense situation with a joke that even bugs laugh at.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+300",
@@ -399,6 +465,8 @@ const data = {
         {
             "name": "Pause Master",
             "description": "Knows when to just step back from the keyboard and breathe.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+300",
@@ -411,6 +479,8 @@ const data = {
         {
             "name": "Coder Healer",
             "description": "Can support a colleague in their darkest hour, even if it’s a 500 error.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+300",
@@ -423,6 +493,8 @@ const data = {
         {
             "name": "Music Code Curator",
             "description": "Creates work playlists so good, even deadlines follow the rhythm.",
+            "img": "../../assets/images/gift-for-harmony.png",
+            "class": "for-harmony",
             "category": "For Harmony",
             "superpowers": {
                 "live": "+300",
@@ -438,6 +510,7 @@ const burger = document.querySelector(".burger");
 const menu = document.querySelector(".mobile-menu");
 const menuLinks = document.querySelectorAll(".mobile-nav-item");
 const body = document.querySelector("body");
+const bestCards = document.querySelector('.best-cards');
 
 // Функция для переключения состояния меню и фона
 function toggleMenu() {
@@ -461,3 +534,47 @@ menuLinks.forEach(function (link) {
         }
     });
 });
+
+// Функция перемешивания массива (алгоритм Фишера-Йейтса)
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// Функция дублирования данных, чтобы перемешать массив
+function prepareData() {
+    const duplicatedItems = [];
+    duplicatedItems.push(...(shuffle([...data.items])));
+    data.items = duplicatedItems;
+    console.log(data.items.name);
+}
+
+
+//Функция заполнения карточек
+function populateBestCards() {
+    prepareData();
+    bestCards.innerHTML = '';
+
+    for (let i = 0; i < 4; i++) {
+        let index = i % data.items.length;
+        const card = document.createElement('div');
+        card.className = 'best-card';
+        card.innerHTML = `
+        <div class="best-card-image"><img src="${data.items[index].img}" alt="${data.items[index].category}"></div>
+        <div class="best-card-description">
+            <div class="best-card-title ${data.items[index].class} text">${data.items[index].category}</div>
+             <h4 class="best-card-subtitle">${data.items[index].name}</h4>
+         </div>
+        `;
+        bestCards.appendChild(card);
+    }
+}
+
+// Обработчик загрузки страницы
+window.addEventListener('load', populateBestCards);
+
+//вызов функции заполнения карточек
+populateBestCards();
